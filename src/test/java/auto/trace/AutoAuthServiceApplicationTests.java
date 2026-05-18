464f257a128e55e90a68161b08e0d42b9f5f5409
andilupu92 <andilupu92@gmail.com>
2026-05-18 20:10:56 +0300

    add test and H2 database

--- src/test/java/auto/trace/AutoAuthServiceApplicationTests.java
+++ src/test/java/auto/trace/AutoAuthServiceApplicationTests.java
@@ -1,8 +1,9 @@
 package auto.trace;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class AutoAuthServiceApplicationTests {
 
 	@Test
