fbe1d4255a6c9843668e86721dc6f9a1a748295c
andilupu92 <andilupu92@gmail.com>
2026-05-18 20:01:35 +0300

    remove spring boot test adnotation

--- src/test/java/auto/trace/AutoAuthServiceApplicationTests.java
+++ src/test/java/auto/trace/AutoAuthServiceApplicationTests.java
@@ -1,9 +1,8 @@
 package auto.trace;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AutoAuthServiceApplicationTests {
 
 	@Test
