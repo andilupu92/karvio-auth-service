40f6ce634d6f4d077989006d0bb6653dbac52f13
andilupu92 <andilupu92@gmail.com>
2026-07-02 13:29:39 +0300

    fix bug in user entity

--- src/main/java/karvio/entity/User.java
+++ src/main/java/karvio/entity/User.java
@@ -5,6 +5,7 @@ import karvio.enums.AuthProvider;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Entity
 @Data
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 public class User {
 
